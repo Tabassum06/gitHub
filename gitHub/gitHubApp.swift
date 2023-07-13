37c3ba9dbf76e0983ca3921bd4d181b20065e4e3
Tabassum Zaman <tabassumzaman39@gmail.com>
2023-07-13 11:25:51 -0700

    Initial Commit

---
+++ gitHub/gitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubApp.swift
+//  gitHub
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct gitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
